c46f2a909f9770e8fe59dc39f00c69f3c4c803e3
Aditya Faiz <159783293+APISXD@users.noreply.github.com>
2025-08-22 16:54:56 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/trackerutbk26/', // <-- pakai nama repo kamu
 })
-
-// =============================================
-// File: postcss.config.js
-// =============================================
